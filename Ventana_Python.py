7e54993b11591727cc78f16221067b7d7d2d6867
felixasela <80848235+felixasela@users.noreply.github.com>
2022-05-11 18:01:40 -0500

    Update Ventana_Python.py

--- Ventana_Python.py
+++ Ventana_Python.py
@@ -183,6 +183,211 @@ def abrBases():
     botonConvertir.place(x = 180, y = 140)
     
 
+    def abrirCombinacion():
+    ventanaComb = Toplevel()
+    ventanaComb.title("Combinaciones")
+    ventanaComb.geometry("500x300")
+    ventanaComb.resizable(False,False)
+
+    miLabel = Label(ventanaComb, text="Realice la combinación a partir de los datos dados", font= (18))
+    miLabel.place(x = "65", y = "10")
+
+    cTotal = Entry(ventanaComb)
+    cTotal.place(x= "250", y = "80")
+    cTotal.config(justify="right")
+
+    cCom = Entry(ventanaComb)
+    cCom.place(x= "300", y = "150")
+    cCom.config(justify="right")
+
+    labelNInicial = Label(ventanaComb, text= "Ingrese el numero total de elementos: ")
+    labelNInicial.place(x= "50", y = "80")
+
+
+    labelNFinal = Label(ventanaComb, text= "Ingrese el numero de elementos que desea combinar: ")
+    labelNFinal.place(x= "50", y = "150")
+
+    
+    def factorial(num): 
+        if num < 0: 
+            messagebox.showinfo(message= "ERROR, un numero es negativo")
+
+        elif num == 0: 
+            return 1
+        else: 
+            fact = 1
+            while(num > 1): 
+                fact *= num 
+                num -= 1
+            return fact 
+
+    def combinacion():
+        proceso = (factorial(int(cTotal.get())))/(factorial(int(cCom.get()))*factorial(int(cTotal.get())-int(cCom.get())))
+        messagebox.showinfo(message= "La combinacion de " + str(cCom.get()) + " en " + str(cTotal.get()) + " es " + str(proceso))
+
+
+
+    botonHallar = Button(ventanaComb, text = "Hallar", command=combinacion) 
+    botonHallar.place(x = 180, y = 220)
+
+def abrirCombinacionR():
+    ventanaCombR = Toplevel()
+    ventanaCombR.title("Combinaciones con repeticion")
+    ventanaCombR.geometry("500x300")
+    ventanaCombR.resizable(False,False)
+
+    miLabel = Label(ventanaCombR, text="Realice la combinación con repeticion a partir de los datos dados", font= (18))
+    miLabel.place(x = "25", y = "10")
+
+    cTotal = Entry(ventanaCombR)
+    cTotal.place(x= "250", y = "80")
+    cTotal.config(justify="right")
+
+    cCom = Entry(ventanaCombR)
+    cCom.place(x= "300", y = "150")
+    cCom.config(justify="right")
+
+    labelNInicial = Label(ventanaCombR, text= "Ingrese el numero total de elementos: ")
+    labelNInicial.place(x= "50", y = "80")
+
+
+    labelNFinal = Label(ventanaCombR, text= "Ingrese el numero de elementos que desea combinar: ")
+    labelNFinal.place(x= "50", y = "150")
+
+    
+    def factorial(num): 
+        if num < 0: 
+            messagebox.showinfo(message= "ERROR, un numero es negativo")
+
+        elif num == 0: 
+            return 1
+        else: 
+            fact = 1
+            while(num > 1): 
+                fact *= num 
+                num -= 1
+            return fact 
+
+    def combinacionR():
+        proceso = (factorial((int(cTotal.get())+int(cCom.get())-1)))/(factorial(int(cCom.get()))*factorial(int(cTotal.get())-1))
+        messagebox.showinfo(message= "La combinacion con repeticion de " + str(cCom.get()) + " en " + str(cTotal.get()) + " es " + str(proceso))
+
+
+
+    botonHallar = Button(ventanaCombR, text = "Hallar", command=combinacionR) 
+    botonHallar.place(x = 180, y = 220)
+
+def abrirPermutacion():
+    ventanaP = Toplevel()
+    ventanaP.title("Permutacion sin repeticion")
+    ventanaP.geometry("500x300")
+    ventanaP.resizable(False,False)
+
+    miLabel = Label(ventanaP, text="Realice la permutacion sin repeticion a partir de los datos dados", font= (18))
+    miLabel.place(x = "25", y = "10")
+
+    cTotal = Entry(ventanaP)
+    cTotal.place(x= "250", y = "80")
+    cTotal.config(justify="right")
+
+    cCom = Entry(ventanaP)
+    cCom.place(x= "300", y = "150")
+    cCom.config(justify="right")
+
+    labelNInicial = Label(ventanaP, text= "Ingrese el numero total de elementos: ")
+    labelNInicial.place(x= "50", y = "80")
+
+
+    labelNFinal = Label(ventanaP, text= "Ingrese el numero de elementos que desea combinar: ")
+    labelNFinal.place(x= "50", y = "150")
+
+    
+    def factorial(num): 
+        if num < 0: 
+            messagebox.showinfo(message= "ERROR, un numero es negativo")
+
+        elif num == 0: 
+            return 1
+        else: 
+            fact = 1
+            while(num > 1): 
+                fact *= num 
+                num -= 1
+            return fact 
+
+    def permutacionR():
+        if cTotal == cCom:
+            proceso = factorial(int(cTotal.get()))
+            messagebox.showinfo(message= "La permutacion de " + str(cCom.get()) + " en " + str(cTotal.get()) + " es " + str(proceso))
+        else:
+            proceso = (factorial((int(cTotal.get()))))/(factorial(int(cTotal.get())-int(cCom.get())))
+            messagebox.showinfo(message= "La permutacion de " + str(cCom.get()) + " en " + str(cTotal.get()) + " es " + str(proceso))
+
+
+
+    botonHallar = Button(ventanaP, text = "Hallar", command=permutacionR) 
+    botonHallar.place(x = 180, y = 220)
+
+def abrirPermutacionR():
+    ventanaPR = Toplevel()
+    ventanaPR.title("Permutacion con repeticion")
+    ventanaPR.geometry("500x400")
+    ventanaPR.resizable(False,False)
+
+    miLabel = Label(ventanaPR, text="Realice la permutacion con repeticion a partir de los datos dados", font= (18))
+    miLabel.place(x = "25", y = "10")
+
+    cTotal = Entry(ventanaPR)
+    cTotal.place(x= "250", y = "80")
+    cTotal.config(justify="right")
+
+    elementos = Entry(ventanaPR)
+    elementos.place(x= "300", y = "150")
+    elementos.config(justify="right")
+
+    elementos2 = Entry(ventanaPR)
+    elementos2.place(x= "300", y = "200")
+    elementos2.config(justify="right")
+
+    elementos3 = Entry(ventanaPR)
+    elementos3.place(x= "300", y = "250")
+    elementos3.config(justify="right")
+
+    labelNInicial = Label(ventanaPR, text= "Ingrese el numero total de elementos: ")
+    labelNInicial.place(x= "50", y = "80")
+
+    labelNFinal = Label(ventanaPR, text= "Ingrese las veces que se repite el primer elemento: ")
+    labelNFinal.place(x= "50", y = "150")
+
+    labelNFinal = Label(ventanaPR, text= "Ingrese las veces que se repite el segundo elemento: ")
+    labelNFinal.place(x= "50", y = "200")
+
+    labelNFinal = Label(ventanaPR, text= "Ingrese las veces que se repite el tercer elemento: ")
+    labelNFinal.place(x= "50", y = "250")
+
+    
+    def factorial(num): 
+        if num < 0: 
+            messagebox.showinfo(message= "ERROR, un numero es negativo")
+
+        elif num == 0: 
+            return 1
+        else: 
+            fact = 1
+            while(num > 1): 
+                fact *= num 
+                num -= 1
+            return fact 
+
+    def permutacionR():
+        proceso = (factorial((int(cTotal.get()))))/(factorial(int(elementos.get()))*factorial(int(elementos2.get()))*factorial(int(elementos3.get())))
+        messagebox.showinfo(message= "La permutacion de " + str(cTotal.get()) + " repitiendo los elementos dados es " + str(proceso))
+
+
+
+    botonHallar = Button(ventanaPR, text = "Hallar", command=permutacionR) 
+    botonHallar.place(x = 180, y = 300)
+    
 labelQueDesea = Label(raiz, text= "¿Que operacion deseas realizar?", font= ("Courier", 10, "italic"))
 labelQueDesea.place(x = 15, y = 20)
 
@@ -194,5 +399,18 @@ botonBases.place(x = 35, y = 110)
 
 botonPrimos = Button(raiz, text = "Conversion de bases", command=abrBases, font= ("Courier", 10, "italic"))
 botonPrimos.place(x = 35, y = 160)
+
+botonComb = Button(raiz, text = "Combinacion sin repeticion", command=abrirCombinacion)
+botonComb.place(x = 165, y = 270)
+
+botonCombR = Button(raiz, text = "Combinacion con repeticion", command=abrirCombinacionR)
+botonCombR.place(x = 165, y = 320)
+
+botonPer = Button(raiz, text = "Permutacion sin repeticion", command=abrirPermutacion)
+botonPer.place(x = 165, y = 370)
+
+botonPerR = Button(raiz, text = "Permutacion con repeticion", command=abrirPermutacionR)
+botonPerR.place(x = 165, y = 420)
+
 
 raiz.mainloop()
